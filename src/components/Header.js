9e3395782bc399835dbd0d42e33b507c8a1bae06
Haru Lab <84611600+HaruLab@users.noreply.github.com>
2025-06-07 12:10:20 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
       <div className="fixed z-50 top-0 left-0 w-full max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
         <Link href="/">
           <img
-            src="/logo_black.png"
+            src="/2025_portfolio_dev/images/main.jpg/logo_black.png"
             alt="HARULAB ロゴ"
             className="h-15 w-auto"
           />
