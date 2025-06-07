888f8b635a8598012e7ea70f0bc7f28409abada6
Haru Lab <84611600+HaruLab@users.noreply.github.com>
2025-06-07 12:05:13 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/2025_portfolio_dev",
+  assetPrefix: "/2025_portfolio_dev",
 };
 
 export default nextConfig;
