afda3ae6b25eab1f1852a1f77b1ca46b4f189fbf
harulab <maeharuharu35@icloud.com>
2025-07-12 10:50:54 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
-const repo = '2025_portfolio_dev';
-const assetPrefix = isGithubActions ? `/${repo}/` : '';
-const basePath = isGithubActions ? `/${repo}` : '';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     remotePatterns: [
       {
